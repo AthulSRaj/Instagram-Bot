d5988a01764433d0059630f45784e7e8e0d9211b
Asura <athulsureshraj@outlook.com>
2025-07-17 13:33:40 -0400

    Update instabot1.py

--- instabot1.py
+++ instabot1.py
@@ -3,7 +3,7 @@ import time
 
 cl = Client()
 
-cl.login('arthurrajking', 'Qwerty@1234')
+cl.login('UserName', 'Password')
 friend_username = 'mariolaynefabrizio'
 
 user_id = cl.user_id_from_username(friend_username)
@@ -12,6 +12,6 @@ medias = cl.user_medias(user_id, 5)
 
 for media in medias:
     cl.media_like(media.id)
-    cl.media_comment(media.id, "wow, I'm on acid and this is wild")
+    cl.media_comment(media.id, "wow, good job")
     print(f"Liked and commented on post: {media.pk}")
     time.sleep(20)
